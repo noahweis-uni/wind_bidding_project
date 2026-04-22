5b5afc217abc02ba3e474c2bc524937f6f5531fa
noahw231 <nowe2004@gmail.com>
2026-04-22 16:05:27 +0200

    deleted not necessary file

--- wind_bidding_project/push_to_github.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Einmalig ausführen um alles ins bestehende Repo zu pushen
-# Vorher: git remote muss schon gesetzt sein
-
-git add .
-git commit -m "feat: complete project skeleton with all notebooks and src modules"
-git push origin main
